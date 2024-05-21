6635a7e6ddee8753e9beee72492ef56e02c94fd1
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2024-05-21 21:49:53 +0545

    Time: 0 ms (100.00%) | Memory: 41.2 MB (67.07%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,20 @@
+//Using Brute Force naive Approach
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+        int m=haystack.length();
+        int n=needle.length();
+
+        for(int i=0; i<=(m-n); i++ ){
+            for(int j=0; j<n; j++){
+
+                    if (haystack.charAt(i + j) != needle.charAt(j))
+                    break;
+
+                    if(j==n-1)
+                    return i;
+            }
+        }
+        return -1;
+    }
+}
