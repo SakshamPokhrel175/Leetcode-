49e54e5704ccc8e6a63a9e4c95006482b5afc68c
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2024-05-28 22:08:56 +0545

    Time: 0 ms (100.00%) | Memory: 44.6 MB (97.85%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,22 +1,13 @@
-//Sum of all elememnts
-
-class Solution {
-    public int missingNumber(int[] nums) { 
-
-        int n=nums.length;
-        int sum=(n*(n+1))/2;
-
-        for(int i=0; i<n; i++)
-        {
-            sum=sum-nums[i];
-        }
-     return sum;   
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n*(n + 1)/2;
+        for(int i : nums){
+            sum-= i;
+        }    
+        return sum;
     }
 }
 
-// Complexity
-// Time complexity:
-// O(n)
 
-// Space complexity:
-// O(1)
+//The line for(int i : nums) is an enhanced for loop (also known as a "foreach" loop) in Java. It iterates over each element in the array nums, assigning each element in turn to the variable i.
