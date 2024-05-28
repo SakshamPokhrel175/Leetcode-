34c251500f29b86921c11df1897c8cab626d2e2b
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2024-05-28 19:35:47 +0545

    Time: 1 ms (100.00%) | Memory: 46 MB (42.83%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,32 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int j=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]!=0){
+//                 int temp=nums[i];
+//                 nums[i]=nums[j];
+//                 nums[j]=temp;
+//                 j++;
+//             }
+//         }
+
+//     }
+// }
+
 class Solution {
     public void moveZeroes(int[] nums) {
+        int n=nums.length;
+
         int j=0;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
+                nums[j]=nums[i];
                 j++;
             }
         }
 
+        for(;j<n;j++){
+            nums[j]=0;
+        }
     }
 }
