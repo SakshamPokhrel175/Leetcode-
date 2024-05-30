967bad5b4d55f3ea9219574b9e818dbc9268640b
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2024-05-30 10:41:23 +0545

    Time: 5 ms (68.34%) | Memory: 57.9 MB (33.91%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -18,3 +18,19 @@ class Solution {
         return maxArea;
     }
 }
+
+
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int maxArea = 0;
+
+//         for (int i = 0; i < height.length; i++) {
+//             for (int j = i + 1; j < height.length; j++) {
+//                 int area = Math.min(height[i], height[j]) * (j - i);
+//                 maxArea = Math.max(maxArea, area);
+//             }
+//         }
+
+//         return maxArea;
+//     }
+// }
