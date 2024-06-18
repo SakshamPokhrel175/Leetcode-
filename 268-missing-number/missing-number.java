adadd24a5782b7bf3d366db0ca246db763d57734
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2024-06-18 21:30:20 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (14.86%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,9 +1,9 @@
 public class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int sum = n*(n + 1)/2;
+        int sum = (n*(n + 1))/2;
         for(int i : nums){
-            sum-= i;
+            sum=sum- i;
         }    
         return sum;
     }
