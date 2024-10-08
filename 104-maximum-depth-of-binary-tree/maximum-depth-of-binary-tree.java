7f7c53eb22b11cf1dacd94439b0031fdebc07b1d
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2024-10-08 07:25:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (26.79%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftdepth=maxDepth(root.left);
+        int rightdepth=maxDepth(root.right);
+
+        return Math.max(leftdepth , rightdepth) +1;
+    }
+}
+
+
+// The +1 is there to count the current node you are at in the tree. Every time you move up one level from a child node to its parent, you need to add 1 to the depth because you're one step higher in the tree.
