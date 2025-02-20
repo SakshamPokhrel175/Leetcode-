bd110cd785b3cc98ac0fbfbdfb56070adc33b202
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2025-02-20 12:40:38 +0545

    Time: 2 ms (8.25%) | Memory: 45.7 MB (15.69%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -1,5 +1,41 @@
 class Solution {
     public int[] buildArray(int[] nums) {
+
+        /** 
+        a=3,b=4,n=6
+        a=a+(b%b)*n;==27
+        a=a%n; ==3
+        b=a/n; ==4
+        
+        
+        */
+
+
+        int n=nums.length;      
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]+(nums[nums[i]]%n)*n;
+        }
+
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]/n;
+        }
+
+        return nums;
+
+
+
+        
+    }
+}
+
+
+
+
+
+
+/*
+class Solution {
+    public int[] buildArray(int[] nums) {
         int[] ans=new int[nums.length];
 
         for(int i=0;i<nums.length;i++){
@@ -11,3 +47,4 @@ class Solution {
         
     }
 }
+*/
