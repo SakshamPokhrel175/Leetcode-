e508a67a87d922a457ad20821092e90f4293e88e
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2025-03-25 09:46:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (76.51%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -30,15 +30,15 @@ class Solution {
         }
     }
 
-    public static void printMatrix(int[][] matrix){
-        for(int[]row : matrix){
-            for(int num : row){
-                System.out.print(num+ " ");
-            }
-            System.out.println();
-
+   public static void printMatrix(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[i].length; j++) { 
+            System.out.print(matrix[i][j] + " "); 
         }
+        System.out.println(); 
     }
+}
+
 
 
 }
