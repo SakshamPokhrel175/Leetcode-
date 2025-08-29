9b30322c54ffe8c3680178355500bfbcb796dd9f
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2025-08-29 11:42:44 +0545

    Time: 14 ms (31.39%) | Memory: 45.1 MB (29.47%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-//Two Pointer
+        //Two Pointer
         //1. Lowercase the string
-        s=s.toLowerCase();
+        s = s.toLowerCase();
         // 2. Remove everything except a-z and 0-9
-        s=s.replaceAll("[^a-z0-9]","");
-        int left=0;
-        int right=s.length()-1;
-        while(left<=right){
-            if(s.charAt(left)!=s.charAt(right)){
-                return false;
-            }else{
+        s = s.replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false; // mismatch → not palindrome
+            } else { // move both pointers inward
                 left++;
                 right--;
             }
         }
 
         return true;
-        
+
     }
 }
