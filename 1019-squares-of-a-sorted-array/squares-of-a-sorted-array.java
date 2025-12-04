dc22141089498c63a18eee25f7f0ce53f637809c
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2025-12-04 23:48:42 +0530

    Time: 3 ms (46.03%) | Memory: 47.9 MB (11.03%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,17 +1,34 @@
+import java.util.*;
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n=nums.length;
-        int i=0,j=0;
-         while(j<n){
-             if(nums[i]==nums[j]){
-                 nums[i]=nums[i]*nums[j];
-                 i++;
-                 j++;
-             }
-         }
-        Arrays.sort(nums);
+        int n = nums.length;
+        int i = 0, j = n - 1;
+        List<Integer> templist = new ArrayList<>();
+        while (i <= j) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                templist.add(nums[i] * nums[i]);
+                i++;
+            } else {
+                templist.add(nums[j] * nums[j]);
+                j--;
+            }
 
-        return nums;
-        
+        }
+
+        // The list is now [100, 16, 9, 1, 0] for the first example.
+        // We must reverse it to be in non-decreasing order.
+        // Collections.reverse operates in O(n) time and reverses the list in place.
+
+        Collections.reverse(templist);
+
+        // Convert the ArrayList<Integer> back to a primitive int[] array.
+        int[] result = new int[n];
+        for (i = 0; i < n; i++) {
+            result[i] = templist.get(i);
+        }
+
+        return result;
+
     }
 }
