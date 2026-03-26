28d400ca1ccfdbad6bc7561b7556b3ec4c29e23b
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-03-26 12:56:54 +0545

    Time: 1 ms (99.95%) | Memory: 47.4 MB (60.64%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,13 +1,29 @@
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         for(int i=0;i<nums.length;i++){
+//             //square
+//             nums[i]=nums[i]*nums[i];
+//         }
+//         Arrays.sort(nums);
+
+//         return nums;
+//     }
+// }
+//this was O(nlogn) so now we do for O(n)
+
+//https://youtu.be/MakXVqKUcug?si=9sGTtK8oyylvqvQE
 class Solution {
     public int[] sortedSquares(int[] nums) {
-
-        int[] result= new int[nums.length];
+        int[] result=new int[nums.length];
         int i=0;
         int j=nums.length-1;
-        int k=nums.length-1;
+        int k=nums.length-1; //result = [_, _, _, _, 100]
+
         while(k>=0){
-            int a=nums[i]*nums[i];
-            int b = nums[j]*nums[j];
+            int a=nums[i]*nums[i];  //square of i value
+            int b=nums[j]*nums[j]; //square of j value
+
+            //compare
             if(a>b){
                 result[k]=a;
                 i++;
@@ -18,7 +34,7 @@ class Solution {
                 k--;
             }
         }
+
         return result;
-        
     }
 }
