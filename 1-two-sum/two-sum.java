30036c30c41762d0e6d550decc4cec7d30a2e01e
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-03-26 10:29:03 +0545

    Time: 2 ms (99.26%) | Memory: 47 MB (67.44%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -11,34 +11,22 @@ class Solution {
         // return new int[]{};
         //this was O(n2)
 
-
-        // int i=0;
-        // int j=i+1;
-
-        // while(j<nums.length){
-        //     if(nums[i]+nums[j]==target){
-        //         return new int[]{i,j};
-        //     }
-        //     i++;
-        //     j++;
-        // }
-        // return new int[]{};
-        //this was O()
-
         //HashMap
 
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int needed=target-nums[i];
-            if(map.containsKey(needed)){
-                return new int[]{map.get(needed),i};
-            }
-            map.put(nums[i],i);
-        }
-        return new int[]{};
-        
 
-        
-        
+        for (int i = 0; i < nums.length; i++) {
+            int needed = target - nums[i];
+
+            if (map.containsKey(needed)) {
+                return new int[] { map.get(needed), i };
+            }
+
+            map.put(nums[i], i);  //store value
+        }
+        return new int[] {};
+
     }
+
+    //this is O(n)
 }
