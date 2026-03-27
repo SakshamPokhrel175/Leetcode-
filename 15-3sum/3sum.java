6026e8a6ef0ffb6abaecaf4179f80903a99d7d36
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-03-27 10:36:11 +0545

    Time: 35 ms (43.44%) | Memory: 58.7 MB (92.99%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,45 +1,39 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
         List<List<Integer>> result = new ArrayList<>();
-        //sort array
         Arrays.sort(nums);
 
-        //remove duplicate
         for (int i = 0; i < nums.length - 2; i++) {
-            //remove duplicate
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
 
-            //two pointer technique
+            // skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
             int left = i + 1;
             int right = nums.length - 1;
 
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
+
                 if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
                     left++;
                     right--;
 
-                    //if duplicate found in left and right also then  SKIPP duplicate
-                    while (left < right && nums[left] == nums[left-1]) {
-                        left++;
-                    }
-                    while (left < right && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-                }else if(sum<0){
+                    // skip duplicates
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+
+                } else if (sum < 0) {
                     left++;
-                }else{
+                } else {
                     right--;
                 }
-
             }
-
         }
-        return result;
 
+        return result;
     }
 }
