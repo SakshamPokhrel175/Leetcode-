f6d18318620c9528ca3d92d561f28a3a22408a04
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-03-29 00:51:59 +0545

    Time: 1 ms (78.16%) | Memory: 47 MB (5.61%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -4,12 +4,15 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             if(nums[i]!=nums[i-1]){
                 nums[unique]=nums[i];
-                unique++;
+                unique ++;
             }
             
         }
+
         return unique;
-        
         
     }
 }
+
+//https://www.geeksforgeeks.org/problems/remove-duplicate-elements-from-sorted-array/1
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
