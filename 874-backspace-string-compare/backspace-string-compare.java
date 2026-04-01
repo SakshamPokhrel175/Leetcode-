49face2ddde7474095d34eb104b89942ebf0e157
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-04-01 10:40:23 +0545

    Time: 2 ms (56.12%) | Memory: 42.7 MB (91.55%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,29 +1,92 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        StringBuilder sb1= new StringBuilder();
-        StringBuilder sb2= new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
 
-        for(int i=0;i<s.length();i++){
-            char c =s.charAt(i);
-            if(c!='#'){
+        // Process s
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != '#') {
                 sb1.append(c);
-            }else if(sb1.length()>0){
-                sb1.deleteCharAt(sb1.length()-1);
+            } else if (sb1.length() > 0) {
+                sb1.deleteCharAt(sb1.length() - 1);
             }
         }
 
-        for(int i=0;i<t.length();i++){
-            char c =t.charAt(i);
-            if(c!='#'){
+        // Process t
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            if (c != '#') {
                 sb2.append(c);
-            }else if(sb2.length()>0){
-                sb2.deleteCharAt(sb2.length()-1);
+            } else if (sb2.length() > 0) {
+                sb2.deleteCharAt(sb2.length() - 1);
             }
-            
         }
 
         return sb1.toString().equals(sb2.toString());
-            
-        
     }
 }
+
+//this is O(n+m)
+
+
+
+
+
+
+
+
+
+
+
+//O(n)
+
+// class Solution {
+//     public boolean backspaceCompare(String s, String t) {
+//         int i = s.length() - 1;
+//         int j = t.length() - 1;
+
+//         int skipS = 0, skipT = 0;
+
+//         while (i >= 0 || j >= 0) {
+
+//             // process s
+//             while (i >= 0) {
+//                 if (s.charAt(i) == '#') {
+//                     skipS++;
+//                     i--;
+//                 } else if (skipS > 0) {
+//                     skipS--;
+//                     i--;
+//                 } else {
+//                     break;
+//                 }
+//             }
+
+//             // process t
+//             while (j >= 0) {
+//                 if (t.charAt(j) == '#') {
+//                     skipT++;
+//                     j--;
+//                 } else if (skipT > 0) {
+//                     skipT--;
+//                     j--;
+//                 } else {
+//                     break;
+//                 }
+//             }
+
+//             // compare characters
+//             if (i >= 0 && j >= 0) {
+//                 if (s.charAt(i) != t.charAt(j)) return false;
+//             } else {
+//                 if (i >= 0 || j >= 0) return false;
+//             }
+
+//             i--;
+//             j--;
+//         }
+
+//         return true;
+//     }
+// }
