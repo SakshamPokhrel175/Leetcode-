5d8d8b3fb823e8858cf4efd54514cc7da188d9cd
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-04-01 14:30:55 +0545

    Time: 4 ms (92.34%) | Memory: 82.9 MB (64.61%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,46 +1,24 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int i=nums[0];;
-        int j=nums[0];
-        i=nums[i];
-        j=nums[nums[j]];
+        int left=nums[0];
+        int right=nums[0];
+        left=nums[left];
+        right=nums[nums[right]];
 
         //detect cycle
-        while(i!=j){
-            i=nums[i];
-            j=nums[nums[j]];
+        while(left!=right){
+            left=nums[left];
+            right=nums[nums[right]];      
         }
 
-        //find duplicate
-        i=nums[0];
-        while(i!=j){
-            i=nums[i];
-            j=nums[j];
+        //duplicate find
+        left=nums[0];
+        while(left!=right){
+            left=nums[left];
+            right=nums[right];
         }
 
+        return left;
         
-
-        return i;
     }
 }
-
-
-
-
-
-
-//o(nlogn)
-// class Solution {
-//     public int findDuplicate(int[] nums) {
-//         Arrays.sort(nums);
-
-//         for (int i = 1; i < nums.length; i++) {
-//             if (nums[i] == nums[i - 1]) {
-//                 return nums[i];
-
-//             }
-//         }
-//         return -1;
-
-//     }
-// }
