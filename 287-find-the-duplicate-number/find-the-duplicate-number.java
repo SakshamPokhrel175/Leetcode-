4a409973ffaf370fd914237df4f030d6bdaa0751
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-04-02 20:27:31 +0545

    Time: 38 ms (12.48%) | Memory: 79 MB (97.35%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,46 +1,13 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int i=nums[0];
-        int j=nums[0];
-        i=nums[i];
-        j=nums[nums[j]];
-
-        //detect cycle
-        while(i!=j){
-            i=nums[i];
-            j=nums[nums[j]];
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                return nums[i];
+            }
         }
 
-        //find duplicate
-        i=nums[0];
-        while(i!=j){
-            i=nums[i];
-            j=nums[j];
-        }
-
+        return -1;
         
-
-        return i;
     }
 }
-
-
-
-
-
-
-//o(nlogn)
-// class Solution {
-//     public int findDuplicate(int[] nums) {
-//         Arrays.sort(nums);
-
-//         for (int i = 1; i < nums.length; i++) {
-//             if (nums[i] == nums[i - 1]) {
-//                 return nums[i];
-
-//             }
-//         }
-//         return -1;
-
-//     }
-// }
