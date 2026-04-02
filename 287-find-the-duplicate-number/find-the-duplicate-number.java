92953eda21aceaf22ea3998b1eb2b5690bee401f
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-04-02 20:42:20 +0545

    Time: 4 ms (92.42%) | Memory: 83.1 MB (32.00%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,24 +1,72 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        int i=nums[0];
+        int j=nums[0];
+        i=nums[i];
+        j=nums[nums[j]];
 
-        int left=nums[0];
-        int right=nums[0];
-
-        left=nums[left];
-        right=nums[nums[right]];
-
-        while(left!=right){
-            left=nums[left];
-            right=nums[nums[right]];
+        //detect cycle
+        while(i!=j){
+            i=nums[i];
+            j=nums[nums[j]];
         }
 
-        left=nums[0];
-        while(left!=right){
-            left=nums[left];
-            right=nums[right];
+        //find duplicate
+        i=nums[0];
+        while(i!=j){
+            i=nums[i];
+            j=nums[j];
         }
 
-        return left;
         
+
+        return i;
     }
 }
+
+// Your code has 2 phases:
+
+// 1️⃣ Detect cycle (meeting point)
+// 2️⃣ Find cycle start (duplicate number)
+
+
+    
+
+// left = nums[0];
+
+// This is the most confusing but important part.
+
+// 💡 Key idea
+
+// When slow & fast meet:
+
+// They are somewhere inside the cycle
+// NOT necessarily at the start
+
+// We want:
+// 👉 start of cycle = duplicate number
+    
+
+// Start → → → → [Cycle Start] → → → → (loop)
+//                   ↑              ↓
+//                   ← ← ← ← ← ← ← ←
+
+
+
+
+
+//o(nlogn)
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 return nums[i];
+
+//             }
+//         }
+//         return -1;
+
+//     }
+// }
