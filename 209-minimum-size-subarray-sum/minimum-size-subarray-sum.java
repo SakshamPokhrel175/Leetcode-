b9dd0d1c9b6bf3b29b482bd8facef788047cc61e
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-04-04 12:44:56 +0545

    Time: 1 ms (99.93%) | Memory: 61.9 MB (78.98%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,50 +1,22 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int low = 0;
-        int sum = 0;
-        int result = Integer.MAX_VALUE;
+        int low=0;
+        int sum=0;
+        int result=Integer.MAX_VALUE;
 
-        for (int high = 0; high < nums.length; high++) {
-            sum += nums[high];
-
-            while (sum >= target) {
-                int length = high - low + 1;
-                result = Math.min(result, length);
-                sum -= nums[low];
+        for(int high=0;high<nums.length;high++){
+            sum=sum+nums[high];
+            while(sum>=target){
+                int length=high-low+1;
+                result=Math.min(result,length);
+                sum=sum-nums[low];
                 low++;
             }
         }
-
-        return result == Integer.MAX_VALUE ? 0 : result;
+        if(result==Integer.MAX_VALUE){
+            return 0;
+        }
+        return result;
+        
     }
 }
-
-
-
-
-// class Solution {
-//     public int minSubArrayLen(int target, int[] nums) {
-//         //hiring and fiering
-//         int low = 0;
-//         int high = 0;
-//         int result = Integer.MAX_VALUE;
-//         int sum = 0;
-
-//         while (high < nums.length) {
-//             sum = sum + nums[high];
-//             while (sum >= target) {
-//                 int length = high - low + 1;
-//                 result = Math.min(result, length);
-//                 sum = sum - nums[low];
-//                 low++;
-//             }
-//             high++;
-//         }
-
-//         if (result == Integer.MAX_VALUE) {
-//             return 0;
-//         }
-//         return result;
-
-//     }
-// }
