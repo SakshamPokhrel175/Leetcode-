b4fffd5c19e517d56b7ea5eeaa85d0c31381707f
Saksham Pokhrel <123399908+SakshamPokhrel175@users.noreply.github.com>
2026-04-05 12:56:19 +0545

    Time: 1 ms (99.93%) | Memory: 62.3 MB (22.79%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,26 +1,50 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left = 0;
-        int min = Integer.MAX_VALUE;
+        int low = 0;
         int sum = 0;
+        int result = Integer.MAX_VALUE;
 
-        for (int right = 0; right < nums.length; right++) {
-            sum = sum + nums[right];
+        for (int high = 0; high < nums.length; high++) {
+            sum += nums[high];
 
             while (sum >= target) {
-                min = Math.min(min, right - left + 1);
-                sum = sum - nums[left];
-
-                left++;
-
+                int length = high - low + 1;
+                result = Math.min(result, length);
+                sum -= nums[low];
+                low++;
             }
-
         }
 
-        if (min == Integer.MAX_VALUE) {
-            return 0;
-        }
-        return min;
-
+        return result == Integer.MAX_VALUE ? 0 : result;
     }
 }
+
+
+
+
+// class Solution {
+//     public int minSubArrayLen(int target, int[] nums) {
+//         //hiring and fiering
+//         int low = 0;
+//         int high = 0;
+//         int result = Integer.MAX_VALUE;
+//         int sum = 0;
+
+//         while (high < nums.length) {
+//             sum = sum + nums[high];
+//             while (sum >= target) {
+//                 int length = high - low + 1;
+//                 result = Math.min(result, length);
+//                 sum = sum - nums[low];
+//                 low++;
+//             }
+//             high++;
+//         }
+
+//         if (result == Integer.MAX_VALUE) {
+//             return 0;
+//         }
+//         return result;
+
+//     }
+// }
